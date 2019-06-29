ed7f8a4427fc567b990d9029cd6a0efb02305a08
Sergei Kazakov <skazakovru@gmail.com>
2019-06-29 19:04:08 +0300

    Initial Commit

---
+++ tabController/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabController
+//
+//  Created by Sergei Kazakov on 6/29/19.
+//  Copyright © 2019 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
